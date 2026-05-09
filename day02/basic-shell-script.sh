2b1b3ed29ab63b58205b00602fca6530693a9032
10-Akshay <akshayrsutar358@gmail.com>
2026-05-09 11:59:11 +0000

    updated changes in shell script day02

--- day02/basic-shell-script.sh
+++ day02/basic-shell-script.sh
@@ -8,6 +8,7 @@ echo "College name is $college and date is $(date)"
 
 
 
+<< comments
 echo "Enter the Username :"
 echo "Enter the Hobbies :"
 echo "Enter the age :"
@@ -19,4 +20,25 @@ read age
 echo "your name is $username"
 echo "your hobby is $hobby"
 echo "your age is $age"
+
+echo "$1 $2 $3"
+
+read -p "Enter 1st value :" 1
+read -p "Enter 2nd value :" 2
+
+sum=$((1+2))
+echo "Addition is :$sum"
+comments
+
+read -p "Enter your name :" username
+read -p "Enter your age :" age
+echo "My name is $username"
+echo "My age is $age"
+
+echo "new arguments are :$0 $1 $2"
+
+sudo useradd -m $username
+echo "user added successfully"
+
+
 
--- day02/create-user-and-add.sh
+++ day02/create-user-and-add.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
+name="Akshay"
+echo "Name is $name"
 
-read -p "Enter the 1st username :" username
-echo "1st Username :$username"
+sudo useradd -m $name 
 
-read -p "Enter the 2nd username :" username2
-echo "2nd username :$username2"
+read -p "Enter your hobby :" hobby
+echo "your hobby is $hobby"
 
-
-sudo useradd -m $username
-sudo useradd -m $username2
-
-
-
-echo "The users are :$0 $1 $2"
-
-
+echo "New :$1 $2"
 

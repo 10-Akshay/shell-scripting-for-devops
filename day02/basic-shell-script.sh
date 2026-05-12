f9534b7881e1cfad8a91dd05763d222572a70339
10-Akshay <akshayrsutar358@gmail.com>
2026-05-12 04:34:51 +0000

    day02 updated shell script

--- day02/basic-shell-script.sh
+++ day02/basic-shell-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<comments
 name="Akshay Sutar"
 echo "The Name is : $name"
 
@@ -8,7 +9,7 @@ echo "College name is $college and date is $(date)"
 
 
 
-<< comments
+
 echo "Enter the Username :"
 echo "Enter the Hobbies :"
 echo "Enter the age :"
@@ -28,7 +29,7 @@ read -p "Enter 2nd value :" 2
 
 sum=$((1+2))
 echo "Addition is :$sum"
-comments
+
 
 read -p "Enter your name :" username
 read -p "Enter your age :" age
@@ -39,6 +40,35 @@ echo "new arguments are :$0 $1 $2"
 
 sudo useradd -m $username
 echo "user added successfully"
+comments
+
+
+read -p "Enter the 1st no :" no1
+read -p "Enter the 2nd no :" no2
+read -p "Enter the 3rd no :" no3
+
+if (( $no1 > $no2 && $no1 > $no3 ))
+then
+	for (( i = 1; i <= 5; i++))
+	do
+		echo "$no1 is Greater"
+	done
+elif (( $no2 > $no1 && $no2 > $no3 ))
+then 
+	for (( i = 1; i <= 5; i++ ))
+	do
+		echo "$no2 is Greater"
+	done
+elif (( $no3 > $no1 && $no3 > $no2 ))
+then
+	for (( i = 1; i <= 5; i++ ))
+	do
+		echo "$no3 is Greater"
+	done
+else
+	echo "Enter valid input"
+fi
+
 
 
 
